3253b47c1308e13fcbb8b01fd2df15d9d0d894c1
hholbroo <hholbroo@unca.edu>
2023-04-11 20:28:30 -0400

    Comments endpoints passing all tests

--- views/comments.py
+++ views/comments.py
@@ -2,17 +2,34 @@ from flask import Response, request
 from flask_restful import Resource
 import json
 from models import db, Comment, Post
+from views import get_authorized_user_ids
 
 class CommentListEndpoint(Resource):
 
     def __init__(self, current_user):
         self.current_user = current_user
+        self.auth_users = get_authorized_user_ids(self.current_user)
+        self.auth_users.append(self.current_user.id)
     
     def post(self):
         # create a new "Comment" based on the data posted in the body 
         body = request.get_json()
+        if body.get('post_id') is None or body.get('text') is None:
+            return "BAD REQUEST: post_id and text required.", 400
+        try:
+            post_id = int(body.get('post_id'))
+            post = Post.query.get(post_id)
+            if post is None or post.user_id not in self.auth_users:
+                raise
+        except ValueError:
+            return f"BAD REQUEST: Unable to access post with id {body.get('post_id')}", 400
+        except:
+            return f"BAD REQUEST: Unable to access post with id {body.get('post_id')}", 404
+        comment = Comment(text=body.get('text'), user_id=self.current_user.id, post_id=post_id)
+        db.session.add(comment)
+        db.session.commit()
         print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+        return Response(json.dumps(comment.to_dict()), mimetype="application/json", status=201)
         
 class CommentDetailEndpoint(Resource):
 
@@ -22,7 +39,14 @@ class CommentDetailEndpoint(Resource):
     def delete(self, id):
         # delete "Comment" record where "id"=id
         print(id)
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        comment = Comment.query.get(id)
+        if comment is None:
+            return f"BAD REQUST: Comment with id {id} not found.", 404
+        if comment.user_id != self.current_user.id:
+            return f"BAD REQUEST: Unauthorized access to comment with id {id}", 404
+        db.session.delete(comment)
+        db.session.commit()
+        return Response(json.dumps({"text": f"Comment {id} deleted"}), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
